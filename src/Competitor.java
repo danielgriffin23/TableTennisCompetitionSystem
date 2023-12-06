12105f4d3329c03ee0aa3f3ab210c25ca1ba9bd3
dg23aay <dg23aay@herts.ac.uk>
2023-12-06 16:11:32 +0000

    Stage Three– Implementing your design

---
+++ src/Competitor.java
@@ -0,0 +1,84 @@
+import java.util.Date;
+
+public class Competitor {
+    private String name;
+    private Date dateOfBirth;
+    private int competitorNumber;
+    private int competitionLevel;
+    private int[] scores;
+    public Competitor(String name, Date dateOfBirth, int competitorNumber, int competitionLevel) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.competitorNumber = competitorNumber;
+        this.competitionLevel = competitionLevel;
+        this.scores = new int[5];
+        System.out.println(name);
+
+    }
+
+    // Getters and setters
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getCompetitorNumber() {
+
+        return competitorNumber;
+    }
+
+    public void setCompetitorNumber(int competitorNumber) {
+
+        this.competitorNumber = competitorNumber;
+    }
+
+    public int getCompetitionLevel() {
+
+        return competitionLevel;
+    }
+
+    public void setCompetitionLevel(int competitionLevel) {
+
+        this.competitionLevel = competitionLevel;
+    }
+
+    // Methods
+
+    //Gets the array of scores
+    public int[] getScoreArray(){
+        return scores;
+    }
+
+    //Calculates the average score of all scores
+    public double getOverallScore() {
+
+        return 5;
+    }
+
+
+    public String getFullDetails() {
+
+        return null;
+    }
+
+    public String getShortDetails() {
+
+        return null;
+    }
+}
---
+++ src/Staff.java
@@ -0,0 +1,20 @@
+public class Staff{
+    private String accessLevel;
+
+    public Staff(String accessLevel) {
+
+        this.accessLevel = accessLevel;
+    }
+
+    // Getters and setters
+
+    public String getAccessLevel() {
+
+        return accessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+
+        this.accessLevel = accessLevel;
+    }
+}
